5ea19db908e3bae639636e61a44cb9c3dd3bc629
hgmorris <113636731+hgmorris@users.noreply.github.com>
2024-01-23 15:31:43 -0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,13 @@
 
-const express = require('express');
-const app = express();
-const contactsRoutes = require('./routes/contacts.js');
+const router = require('express').Router();
+const contactsRoutes = require('./contact');
 
 // Use the contacts routes
-app.use('/contacts', contactsRoutes);
+router.use('/contacts', contactsRoutes);
 
 // Default route
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.json({ message: 'Morris' });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -7,23 +7,12 @@ const app = express();
 
 const { initDb } = require('./mongolia/mongo.js');
 
-// Define a route for the root URL ('/')
-app.get('/', (req, res) => {
-    res.send('Morris!');
-});
-
 // Create an instance of the Express Router
-const router = express.Router("/routes/index.js");
-
-// Define a route for the root URL ('/') within the router
-router.get('/', (req, res) => {
-    res.json({ message: 'Morris' });
-});
+const router = require('./routes/index.js');
+app.use('/', router);
 
 // Set the port for the server. If the environment variable 'Port' is not set, default to port 3000
 const port = process.env.PORT || 3000;
-
-app.use('/', require('./routes/contact.js'));
 
 initDb((err) => {
     if (err) {
