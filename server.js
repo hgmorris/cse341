daf36094413281f9cb95befdcd1caa6cc6d5384d
hgmorris <113636731+hgmorris@users.noreply.github.com>
2024-02-04 15:15:09 -0700

    edit

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger-output.json')
+
 // Import the express module
 const express = require('express');
 
@@ -9,6 +12,19 @@ const { initDb } = require('./mongolia/mongo.js');
 
 // Create an instance of the Express Router
 const router = require('./routes/index.js');
+// Add support for CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+  });
 app.use('/', router);
 
 // Set the port for the server. If the environment variable 'Port' is not set, default to port 3000
@@ -23,3 +39,4 @@ initDb((err) => {
         });
     }
 });
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
